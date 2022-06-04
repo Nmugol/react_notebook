bea6a52c45fdc27813c3bcc913d03e83414fc518
Nmugol <nmugol000@gmail.com>
2022-06-04 17:24:34 +0200

    rebild app

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,34 +1,23 @@
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
-import { Button } from 'react-bootstrap';
-import Menu from './Component/Navbar';
-import About from "./Component/About";
-import Home from "./Component/Home";
-import { Routes, Route } from "react-router-dom";
-import Note from './Component/Note';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [darcMode, setDarkMode] = useState(false);
-
   return (
-    <div className={(darcMode ? 'dark-mode' : 'light-mode') + ' background'}>
-      <style>{"body { background-color:" + (darcMode ? '#46526b' : '#cde8f0') + "; }"}</style>
-      <Menu theme={darcMode} />
-      <div className="container">
-        <div className="setTheme">
-          <Button onClick={() => { setDarkMode(!darcMode) }} variant={darcMode ? "dark" : "light"} size='lg' >
-            <FontAwesomeIcon icon={darcMode ? faSun : faMoon} />
-          </Button>
-        </div>
-        <div>
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={<Home theme={darcMode} />} />
-            <Route path='/new-note' element={<Note theme={darcMode} />} />
-          </Routes>
-        </div>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
